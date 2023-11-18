922aac731e6c2cf56c9f11537a8c7c89be55fd04
onlyVishesh <Visheshmaurya14@gmail.com>
2023-11-18 21:49:39 +0530

    Navbar and footer added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/home/Home.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <h1>Error</h1> ,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <h1>About</h1> ,
+      },
+      {
+        path: "/service",
+        element: <h1>Service</h1> ,
+      },
+      {
+        path: "/hiring",
+        element: <h1>Join Us</h1> ,
+      },
+      {
+        path: "/login",
+        element: <h1>Login</h1> ,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,8 @@
+
+
+const Home = () => {
+  return <div style={{marginTop: '9vh'}}> <h1>Home</h1>
+  </div>;
+};
+
+export default Home;
