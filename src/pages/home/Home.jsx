e9650b5a1e4e394159ee6da6b95a26f49556cc0b
onlyVishesh <Visheshmaurya14@gmail.com>
2023-11-19 18:53:32 +0530

    Added Vision and Experience section to home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,17 @@
+import Experience from "./Experience";
 import Statistics from "./Statistics";
 import Title from "./Title";
+import Vision from "./Vision";
 
 const Home = () => {
-  return <div style={{marginTop: '9vh'}}>
-    <Title/>
-    <Statistics/>
-  </div>;
+  return (
+    <div style={{ marginTop: "9vh" }}>
+      <Title />
+      <Statistics />
+      <Vision />
+      <Experience/>
+    </div>
+  );
 };
 
 export default Home;
